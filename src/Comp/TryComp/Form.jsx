31aef76f23be3427deb34f70c5e0f0b07041e5c5
ChayaIsraelson <i0504188896@gmail.com>
2025-05-28 16:55:19 +0300

    Connecting to the manager and user node

--- src/Comp/TryComp/Form.jsx
+++ src/Comp/TryComp/Form.jsx
@@ -14,30 +14,26 @@ export default function Form() {
     });
 
     const handleSubmit = (e) => {
-        //צריך לטפל בהתחברות עם נוד+מונגו
         e.preventDefault();
+         axios.get(`http://localhost:8080/users/getUser/${formData.passwordOrPhon}`)
+         .then((response)=>{
+            const user=response.data;
+            if(user&&formData.userName===user.userName)
+                navigate("/UserLoans");
+            else
+                alert("שם מנוי או ססמה לא נכונים");
+         })
+        .catch((error)=>{
+            console.error("Error fetching user:", error.response ? error.response.data : error.message);
+            alert("אירעה שגיאה במהלך ההתחברות")
+        }) 
+        
         if (formData.userName === "חיה" && formData.passwordOrPhon === "1234") {
             navigate("/UserLoans");
         } else {
-            alert("שם משתמש או ססמה לא נכונים");
+            alert("שם מנוי או ססמה לא נכונים");
         }
     };
-    // const handleSubmit = async(e) => {
-    //     e.preventDefault();
-    //     try {
-    //         const response = await axios.get(`http://localhost:8080/getUser/${formData.passwordOrPhon}`);
-    //         const user = response.data;
-
-    //         if (user && formData.userName === user.userName) {
-    //             navigate("/UserLoans");
-    //         } else {
-    //             alert("שם משתמש או ססמה לא נכונים");
-    //         }
-    //     } catch (error) {
-    //         console.error('Error fetching user:', error.response ? error.response.data : error.message);
-    //         alert("אירעה שגיאה במהלך התחברות");
-    //     }
-    // };
 
     return (
         <form
@@ -47,7 +43,7 @@ export default function Form() {
         >
 
             <div>
-                <label className="text-sm font-medium">שם משתמש</label>
+                <label className="text-sm font-medium">שם מנוי</label>
                 <Input
                     required
                     value={formData.userName}
@@ -55,7 +51,7 @@ export default function Form() {
                 />
             </div>
             <div>
-                <label className="text-sm font-medium">ססמה /מספר טלפון</label>
+                <label className="text-sm font-medium">מספר טלפון</label>
                 <Input
                     required
                     value={formData.passwordOrPhon}
@@ -63,9 +59,6 @@ export default function Form() {
                 />
             </div>
             <div className="flex justify-end gap-3 pt-4">
-                {/* <Button type="button" variant="outline" onClick={onCancel}>
-                    ביטול
-                </Button> */}
                 <Button
                     type="submit"
                     className="bg-blue-600 hover:bg-blue-700 mx-auto block"
